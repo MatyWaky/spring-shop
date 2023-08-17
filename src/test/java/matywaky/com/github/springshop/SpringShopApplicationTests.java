5c6ccc1bc1e364358bf7fbb1f07eb50665cd40db
Mateusz Nowak <mateusz00nowak@gmail.com>
2023-08-17 13:50:45 +0200

    New project begins

---
+++ src/test/java/matywaky/com/github/springshop/SpringShopApplicationTests.java
@@ -0,0 +1,13 @@
+package matywaky.com.github.springshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
